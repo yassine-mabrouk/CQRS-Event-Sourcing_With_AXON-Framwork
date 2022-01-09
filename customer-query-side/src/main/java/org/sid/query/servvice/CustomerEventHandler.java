76f771f3aaef99ea055513af09b1f994b674ee6e
yassine-mabrouk <mabroukyassine70@gmail.com>
2022-01-09 02:08:05 +0100

    update customer querySide

--- customer-query-side/src/main/java/org/sid/query/servvice/CustomerEventHandler.java
+++ customer-query-side/src/main/java/org/sid/query/servvice/CustomerEventHandler.java
@@ -3,8 +3,10 @@ package org.sid.query.servvice;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.queryhandling.QueryHandler;
 import org.sid.coreapi.commands.CreatedCustomerEvent;
+import org.sid.coreapi.commands.UpdatedCustomerEvent;
 import org.sid.query.entities.Customer;
 import org.sid.query.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
@@ -20,12 +22,21 @@ public class CustomerEventHandler
     @EventHandler
     public  void on (CreatedCustomerEvent event){
         log.info("======================");
-        log.info("Event registred");
+        log.info("CreatedCustomerEvent received");
         Customer customer=new Customer();
         customer.setCustomerId(event.getId());
         customer.setName(event.getName());
         customer.setEmail(event.getEmail());
         customerRepo.save(customer);
+    }
+    @EventSourcingHandler
+    public void on (UpdatedCustomerEvent event){
+        log.info("======================");
+        log.info("UpdatedCustomerEvent received ");
+      Customer customer=customerRepo.findById(event.getId()).get();
+      customer.setName(event.getName());
+      customer.setEmail(event.getEmail());
+      customerRepo.save(customer);
     }
 
 }
